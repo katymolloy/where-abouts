6c66e4ca6a3c1011ceda0302622bb3d5cc548e83
katyisonline <katyxmolloy@gmail.com>
2023-12-05 10:06:28 -0500

    Initial commit
    Set up stack navigation

---
+++ App.js
@@ -0,0 +1,31 @@
+import 'react-native-gesture-handler';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from './screens/home';
+import LogIn from './screens/login';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} options={{ title: 'Home' }} />
+          <Stack.Screen name='LogIn' component={LogIn} options={{ title: 'Log In' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+
+export default function Home({ navigation }) {
+    return(
+        <View>
+        <Text>
+            In here
+        </Text>
+    </View>
+    )
+ 
+}
