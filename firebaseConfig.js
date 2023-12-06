5c3be0c2fefbcbde4a849c25a7edd15d06601f74
katyisonline <katyxmolloy@gmail.com>
2023-12-06 07:40:09 -0500

    New branch for next firebase attempt

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,30 +1,18 @@
-import {initializeApp, getApp, getApps} from 'firebase/app';
-import { getDatabase }  from 'firebase/database';
-import {getAuth} from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
-// Firebase Config
-var firebaseConfig = {
-    apiKey: "AIzaSyBgonsOSbS-cpnNVFyNzyVl1WQZszk2GZM",
-    authDomain: "md---group-project.firebaseapp.com",
-    projectId: "md---group-project",
-    storageBucket: "md---group-project.appspot.com",
-    messagingSenderId: "160922574715",
-    appId: "1:160922574715:web:c735319179eb7dca2a67de",
-    measurementId: "G-7422DW7MF6"
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpYJuE-_l7ZExvn0-ohvWMV-wHnVLWZmY",
+  authDomain: "where-abouts-c7d80.firebaseapp.com",
+  databaseURL: "https://where-abouts-c7d80-default-rtdb.firebaseio.com",
+  projectId: "where-abouts-c7d80",
+  storageBucket: "where-abouts-c7d80.appspot.com",
+  messagingSenderId: "781958942218",
+  appId: "1:781958942218:web:dca176b56f761aad48d9f3"
 };
 
-
-var app;
-if (!getApps().length){
-  app = initializeApp(firebaseConfig); // If no app exists.
-}
-else{
-  const APPS = getApps();
-  app = APPS[0]; // Choose the first app from the array.
-}
-
-export const db = getDatabase(app);
-export const auth = getAuth(app);
-export const firestore = getFirestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
